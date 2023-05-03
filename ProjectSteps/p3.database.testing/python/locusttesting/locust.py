75bde7330f00e7606fcafdba5d59ed8ff9e63084
Juangui37 <76864633+Juangui37@users.noreply.github.com>
2023-05-03 02:31:43 -0400

    Update locust.py

--- ProjectSteps/p3.database.testing/python/locusttesting/locust.py
+++ ProjectSteps/p3.database.testing/python/locusttesting/locust.py
@@ -2,7 +2,7 @@ from locust import HttpUser, task
 from pymongo import MongoClient
 
 # Connect to MongoDB Atlas
-client = MongoClient("mongodb+srv://juangui37:Pacho1414@cluster0.eobads9.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1'), tls=True, tlsAllowInvalidCertificates=True))
+client = MongoClient(<MongoDBString>, server_api=ServerApi('1'), tls=True, tlsAllowInvalidCertificates=True))
 db = client.housing
 propertyinfo = db.propertyinfo
 
